095873b9c3a89f53819db0eaa447fb224cccb7a3
YongTing <jiangyongting0810@gmail.com>
2023-06-04 18:32:40 +0800

    初始化 aside 的 html

--- PulseUI/src/views/Home.vue
+++ PulseUI/src/views/Home.vue
@@ -5,8 +5,8 @@
       <h1>PULSEUI</h1>
       <h2>一个帅气的 UI 框架</h2>
       <p class="actions">
-        <a>Github</a>
-        <a>开始</a>
+        <a href="https://github.com">GitHub</a>
+        <router-link to="/doc">开始</router-link>
       </p>
     </div>
   </div>
