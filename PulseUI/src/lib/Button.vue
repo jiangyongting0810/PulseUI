7b2c45c158137f18b8b55155dd9a2820ac3f7d46
YongTing <jiangyongting0810@gmail.com>
2023-06-11 19:54:03 +0800

    CSS最小影响原则

--- PulseUI/src/lib/Button.vue
+++ PulseUI/src/lib/Button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="pulse-button" :class="`theme-${theme}`">
+  <button class="pulse-button" :class="{[`pulse-theme-${theme}`]: theme}">
     <slot />
   </button>
 </template>
@@ -12,7 +12,7 @@ export default{
   },
 }
 </script>
-<style scoped lang="scss">
+<style lang="scss">
 $h: 32px;
 $border-color: #d9d9d9;
 $color: #333;
--- PulseUI/src/lib/Switch.vue
+++ PulseUI/src/lib/Switch.vue
@@ -1,5 +1,5 @@
 <template>
-  <button @click="toggle" :class="{checked:value}">
+  <button class="pulse-switch" @click="toggle" :class="{'pulse-checked':value}">
     <span>
     </span>
   </button>
@@ -23,7 +23,7 @@ export default{
 <style lang="scss" scoped>
 $h:22px;
 $h2:$h - 4px;
-  button{
+  .pulse-switch{
     height: $h;
     width: $h*2;
     border: none;
@@ -31,29 +31,31 @@ $h2:$h - 4px;
     border-radius: $h / 2;
     position: relative;
     box-shadow: inset 0px 0px 0px 1px rgb(255, 255, 255);
+    > span{
+      position: absolute;
+      top: 2px;
+      left: 2px;
+      height: $h2;
+      width: $h2;
+      background:rgb(255, 255, 255);
+      border-radius: $h2 / 2;
+      box-shadow: inset 0px 0px 10px #D5F1EC;
+      transition:left 0.25s;
+    }
+    &.pulse-checked{
+      background: rgb(74 , 165, 140);
+      box-shadow: inset 0px 0px 0px 1px #327261;
+      > span {
+        left: calc(100% - #{$h2} - 2px);
+      }
+    }
     &:active{
       > span {width: $h2 + 4px;}
     }
-    &.checked:active{
-    > span {width: $h2 + 4px; margin-left: -4px;}
+    &.pulse-checked:active{
+      > span {width: $h2 + 4px; margin-left: -4px;}
+    }
   }
-  }
-  span{
-    position: absolute;
-    top: 2px;
-    left: 2px;
-    height: $h2;
-    width: $h2;
-    background:rgb(255, 255, 255);
-    border-radius: $h2 / 2;
-    box-shadow: inset 0px 0px 10px #D5F1EC;
-    transition:left 0.25s;
-  }
-  button.checked{
-    background: rgb(74 , 165, 140);
-    box-shadow: inset 0px 0px 0px 1px #327261;
-  }
-  button.checked > span {
-    left: calc(100% - #{$h2} - 2px);
-  }
+  
+  
 </style>
