d3354201c83cbd08e9a7d389fcc5977418ea62e1
YongTing <jiangyongting0810@gmail.com>
2023-06-11 13:53:07 +0800

    创建 button 组件

--- PulseUI/src/lib/Button.vue
+++ PulseUI/src/lib/Button.vue
@@ -1,5 +1,21 @@
 <template>
-  <button>
-    <slot />
-  </button>
-</templatw
+  <div>
+    <button v-bind="rest">
+      <slot />
+    </button>
+  </div>
+</template>
+<script lang="ts">
+export default{
+  inheritAttrs:false,
+  setup(props,context){
+    const {size , ...rest} = context.attrs;
+    return {size , rest}
+  }
+}
+</script>
+<style scoped lang="scss">
+  div{
+    border: 1px solid red;
+  }
+</style>
