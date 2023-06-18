30f822722c75eb5a1b044b05f075b309ceb8c239
YongTing <jiangyongting0810@gmail.com>
2023-06-18 11:56:34 +0800

    创建 Tabs 和 Tab 组件

--- PulseUI/src/components/TabsDemo.vue
+++ PulseUI/src/components/TabsDemo.vue
@@ -1,5 +1,22 @@
 <template>
   <div>
-    TabsDemo
+    Tabs示例
   </div>
+  <h1>示例1</h1>
+  <Tabs>
+    <Tab>内容1</Tab>
+    <Tab>内容2</Tab>
+    <Tab>内容3</Tab>
+  </Tabs>
 </template>
+<script lang="ts">
+import Tabs from '../lib/Tabs.vue'
+import Tab from '../lib/Tab.vue'
+
+export default{
+  components:{
+    Tabs,
+    Tab
+  }
+}
+</script>
