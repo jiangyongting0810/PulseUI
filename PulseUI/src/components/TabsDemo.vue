e4c39b5d914c746f0443497f81e74089c6b4ee40
YongTing <jiangyongting0810@gmail.com>
2023-06-19 22:21:49 +0800

    使用 CSS 切换内容

--- PulseUI/src/components/TabsDemo.vue
+++ PulseUI/src/components/TabsDemo.vue
@@ -3,7 +3,7 @@
     Tabs示例
   </div>
   <h1>示例1</h1>
-  <Tabs>
+  <Tabs v-model:selected="x">
     <Tab title="导航1">内容1</Tab>
     <Tab title="导航2">内容2</Tab>
   </Tabs>
@@ -11,11 +11,19 @@
 <script lang="ts">
 import Tabs from '../lib/Tabs.vue'
 import Tab from '../lib/Tab.vue'
-
-export default{
-  components:{
+import {
+  ref
+} from 'vue'
+export default {
+  components: {
     Tabs,
     Tab
+  },
+  setup() {
+    const x = ref('导航1')
+    return {
+      x
+    }
   }
 }
 </script>
--- PulseUI/src/lib/Tabs.vue
+++ PulseUI/src/lib/Tabs.vue
@@ -1,62 +1,81 @@
 <template>
- <div class="pulse-tabs">
-  <div class="pulse-tabs-nav">
-    <div class="pulse-tabs-nav-item" v-for="(t,index) in titles" :key="index">{{t}}</div>
+  <div class="pulse-tabs">
+   <div class="pulse-tabs-nav">
+     <div class="pulse-tabs-nav-item" v-for="(t,index) in titles" @click="select(t)" :class="{selected: t=== selected}" :key="index">{{t}}</div>
   </div>
-  <div class="pulse-tabs-content">
-    <component class="pulse-tabs-content-item" v-for="(c,index) in defaults" :is="c" :key="index" />
-  </div>
-</div>
-</template>
-<script lang="ts">
-import Tab from './Tab.vue'
-export default{
-  setup(props,context){
-    const defaults = context.slots.default?.()
-    if(defaults){
-      defaults.forEach((tag)=>{
-        if(tag.type !== Tab){
-          throw new Error('Tabs 子标签必须是 Tab')
-        }
-      })
-      const titles = defaults.map((tag)=>{
-        return tag.props?.title
-      })
-      return {
-        defaults,
-        titles
-      }
-    }
-    return {
-      defaults: [],
-      titles: [],
-    };
-  }
-}
-</script>
-<style lang="scss">
-$blue: #40a9ff;
-$color: #333;
-$border-color: #d9d9d9;
-.pulse-tabs {
-  &-nav {
-    display: flex;
-    color: $color;
-    border-bottom: 1px solid $border-color;
-    &-item {
-      padding: 8px 0;
-      margin: 0 16px;
-      cursor: pointer;
-      &:first-child {
-        margin-left: 0;
-      }
+   <div class="pulse-tabs-content">
+     <component class="pulse-tabs-content-item" :class="{selected:c.props.title === selected}"  v-for="c in defaults" :is="c" />
+   </div>
+ </div>
+ </template>
+ <script lang="ts">
+import { computed } from 'vue';
+ import Tab from './Tab.vue';
+ export default{
+   props:{
+     selected:{
+       type:String,
+     },
+   },
+   setup(props,context){
+     const defaults = context.slots.default?.()
+     if(defaults){
+       defaults.forEach((tag)=>{
+         if(tag.type !== Tab){
+           throw new Error('Tabs 子标签必须是 Tab')
+         }
+       })
+       const current = computed(()=>{
+        console.log('重新return')
+        return defaults.filter((tag)=>{
+          return tag.props.title === props.selected
+        })[0]
+       })
+       const titles = defaults.map((tag)=>{
+         return tag.props?.title
+       })
+       const select = (title: string) => {
+         context.emit('update:selected', title)
+       }
+       return {
+         defaults,
+         titles,
+         current,
+         select
+       }
+     }
+   }
+ }
+ </script>
+ <style lang="scss">
+ $blue: #40a9ff;
+ $color: #333;
+ $border-color: #d9d9d9;
+ .pulse-tabs {
+   &-nav {
+     display: flex;
+     color: $color;
+     border-bottom: 1px solid $border-color;
+     &-item {
+       padding: 8px 0;
+       margin: 0 16px;
+       cursor: pointer;
+       &:first-child {
+         margin-left: 0;
+       }
+       &.selected {
+         color: $blue;
+       }
+     }
+   }
+   &-content {
+     padding: 8px 0;
+     &-item {
+      display: none;
       &.selected {
-        color: $blue;
+        display: block;
       }
     }
-  }
-  &-content {
-    padding: 8px 0;
-  }
-}
-</style>
+   }
+ }
+ </style>
