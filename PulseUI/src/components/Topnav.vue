c3f26d6ff5ef33cb1cf5bb49ba7b7f39aa4fbb21
YongTing <jiangyongting0810@gmail.com>
2023-06-25 23:26:37 +0800

    features 样式

--- PulseUI/src/components/Topnav.vue
+++ PulseUI/src/components/Topnav.vue
@@ -2,8 +2,9 @@
   <div class="topnav">
       <div class="logo">LOGO</div>
       <ul class="menu">
-        <li>菜单1</li>
-        <li>菜单2</li>
+        <li>
+          <router-link to="/doc">文档</router-link>
+        </li>
       </ul>
       <span class="toggleAside" @click="toggleMenu"></span>
     </div>
