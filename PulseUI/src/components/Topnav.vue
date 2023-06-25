a29c1238a541d03a5016555b78c6e6ec6df7b822
YongTing <jiangyongting0810@gmail.com>
2023-06-25 19:37:55 +0800

    背景渐变

--- PulseUI/src/components/Topnav.vue
+++ PulseUI/src/components/Topnav.vue
@@ -25,7 +25,6 @@ export default{
 <style lang="scss" >
 
 .topnav{
-    background: pink;
     display: flex;
     padding: 16px;
     // position: relative;
--- PulseUI/src/views/Home.vue
+++ PulseUI/src/views/Home.vue
@@ -1,46 +1,51 @@
 <template>
-  <div>
+  <div class="topnavAndBanner">
     <Topnav />
     <div class="banner">
-      <h1>PULSEUI</h1>
-      <h2>一个帅气的 UI 框架</h2>
+      <h1>PulseUI</h1>
+      <h2>一个厉害的 UI 框架</h2>
       <p class="actions">
         <a href="https://github.com">GitHub</a>
         <router-link to="/doc">开始</router-link>
       </p>
     </div>
   </div>
-</template>
-
-<script lang="ts">
-import { inject, Ref } from 'vue'
-import Topnav from '../components/Topnav.vue'
-export default{
-  components:{Topnav},
+  </template>
   
-}
-</script>
-
-<style lang="scss" scoped>
-.banner {
-  padding: 100px 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  background: lightgreen;
-  > .actions{
-    padding: 8px 0;
-    a {
-      margin:0 8px;
-      background: #fff;
-      display: inline-block;
-      $h: 28px;
-      height: $h;
-      line-height: $h;
-      border-radius: $h/2;
-      padding: 0 8px;
+  <script lang="ts">
+  import Topnav from "../components/Topnav.vue";
+  export default {
+    components: {
+      Topnav
+    },
+  };
+  </script>
+  
+  <style lang="scss" scoped>
+  .topnavAndBanner {
+    background: linear-gradient(145deg, rgba(227, 255, 253, 1) 0%, rgba(183, 233, 230, 1) 100%);
+  }
+  
+  .banner {
+    padding: 100px 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  
+    >.actions {
+      padding: 8px 0;
+  
+      a {
+        margin: 0 8px;
+        background: #fff;
+        display: inline-block;
+        $h: 28px;
+        height: $h;
+        line-height: $h;
+        border-radius: $h/2;
+        padding: 0 8px;
+      }
     }
   }
-}
-</style>
+  </style>
