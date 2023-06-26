98c1f9cfef9d10a719aff9250ecd795b91fc3930
YongTing <jiangyongting0810@gmail.com>
2023-06-26 21:54:34 +0800

    复杂一点的高亮

--- PulseUI/src/views/Doc.vue
+++ PulseUI/src/views/Doc.vue
@@ -48,9 +48,7 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-.router-link-active {
-  color: red;
-}
+
 .layout {
   display: flex;
   flex-direction: column;
@@ -84,18 +82,26 @@ export default {
 aside {
   background: lightblue;
   width: 150px;
-  padding: 16px;
   position: fixed;
+  padding: 16px 0;
   top: 0;
   left: 0;
   padding-top: 70px;
   height: 100%;
   > h2 {
     margin-bottom: 4px;
+    padding: 0 16px;
   }
   > ol {
     > li {
-      padding: 4px 0;
+      >a{
+        display: block;
+        padding: 4px 16px;
+        text-decoration: none;
+      }
+      .router-link-active {
+        background: white;
+      }
     }
   }
 }
