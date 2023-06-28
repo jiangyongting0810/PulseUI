a1d80c93cc887fb093700199ec5cc197db7855ad
YongTing <jiangyongting0810@gmail.com>
2023-06-28 21:22:10 +0800

    事不过三：优化 markdown

--- PulseUI/src/router.ts
+++ PulseUI/src/router.ts
@@ -9,7 +9,10 @@ import DocDemo from "./components/DocDemo.vue";
 import Intro from "./views/Intro.vue"
 import GetStarted from "./views/GetStarted.vue"
 import Install from "./views/Install.vue"
+import { h } from 'vue';
+import Markdown from './components/Markdown.vue';
 
+const md =filename =>h(Markdown,{path:`../markdown/${filename}.md`,key: filename})
 const history = createWebHashHistory()
 export const router = createRouter({
   history:history,
@@ -17,9 +20,9 @@ export const router = createRouter({
     {path:'/',component:Home},
     {path:'/doc',component:Doc,children:[
       { path: "", component: DocDemo },
-      { path:"intro",component:Intro },
-      { path:"install",component:Install },
-      { path:"get-started",component:GetStarted },
+      { path: "intro", component: md('intro') },
+      { path: "get-started", component: md('get-started') },
+      { path: "install", component: md('install') },
       { path: "switch", component: SwitchDemo },
       { path: "button", component: ButtonDemo },
       { path: "dialog", component: DialogDemo },
