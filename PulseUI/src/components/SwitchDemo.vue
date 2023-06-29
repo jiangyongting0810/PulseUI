fa3bc0079344fb47f56321738cb31c9b65c726a7
YongTing <jiangyongting0810@gmail.com>
2023-06-29 20:12:17 +0800

    删掉无用代码

--- PulseUI/src/components/SwitchDemo.vue
+++ PulseUI/src/components/SwitchDemo.vue
@@ -1,30 +1,21 @@
 <template>
   <div>
     <h1>Switch 组件示例 </h1>
-    <Switch1Demo/>
     <Demo :component="Switch1Demo" />
     <Demo :component="Switch2Demo" />
-  </div>
+ </div>
 </template>
 
 <script lang="ts">
-import Switch from '../lib/Switch.vue'
-import Button from '../lib/Button.vue'
 import Switch1Demo from './Switch1.demo.vue'
 import {
   ref
 } from 'vue'
 import Switch2Demo from './Switch2.demo.vue'
-import 'prismjs';
-import 'prismjs/themes/prism.css'
 import Demo from './Demo.vue'
-
-const Prism = (window as any).Prism
 
 export default {
   components: {
-    Switch,
-    Button,
     Switch1Demo,
     Switch2Demo,
     Demo
@@ -32,10 +23,8 @@ export default {
   setup() {
     const bool = ref(false)
     return {
-      bool,
       Switch1Demo,
       Switch2Demo,
-      Prism
     }
   }
 }
