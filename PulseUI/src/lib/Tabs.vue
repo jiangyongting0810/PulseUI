cca861ca5b22a601678c92c49a7f6257aec5f516
jiang <jiangyongting0810@gmail.com>
2023-07-11 17:21:58 +0800

    改用vite 构建

--- PulseUI/src/lib/Tabs.vue
+++ PulseUI/src/lib/Tabs.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="gulu-tabs">
-    <div class="gulu-tabs-nav" ref="container">
-      <div class="gulu-tabs-nav-item" v-for="(t, index) in titles"
+  <div class="pulse-tabs">
+    <div class="pulse-tabs-nav" ref="container">
+      <div class="pulse-tabs-nav-item" v-for="(t, index) in titles"
         :ref="el => { if (t === selected) selectedItem = el }" @click="select(t)" :class="{ selected: t === selected }"
         :key="index">{{ t }}</div>
-      <div class="gulu-tabs-nav-indicator" ref="indicator"></div>
+      <div class="pulse-tabs-nav-indicator" ref="indicator"></div>
     </div>
-    <div class="gulu-tabs-content">
+    <div class="pulse-tabs-content">
       <component :is="current" :key="current.props.title" />
     </div>
   </div>
@@ -77,7 +77,7 @@ $blue: #40a9ff;
 $color: #333;
 $border-color: #d9d9d9;
 
-.gulu-tabs {
+.pulse-tabs {
   &-nav {
     display: flex;
     color: $color;
